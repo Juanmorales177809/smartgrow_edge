c8c685f7e28e61baf72f93a2a211393985ee20c0
JuanMorales43 <Mrwokky_43@hotmail.com>
2023-06-27 13:28:39 -0500

    SCD40 mqtt comunication

--- SCD40_Http.ino
+++ SCD40_Http.ino
@@ -1,13 +1,13 @@
 #include <WiFi.h>
-#include <WiFiMulti.h>
-#include <HTTPClient.h>
+#include <PubSubClient.h>
 #include <Wire.h>
 #include "SparkFun_SCD4x_Arduino_Library.h"
 #include <Adafruit_SleepyDog.h> 
 
 SCD4x mySensor;
 
-WiFiMulti wifiMulti;
+WiFiClient esp32Client;
+PubSubClient mqttClient(esp32Client);
 
 //Definir red Wifi a conectar el dispositivo
 //const char* ssid = "smartgrow";
@@ -19,59 +19,87 @@ WiFiMulti wifiMulti;
 const char* ssid = "WIFI-ITM";
 const char* password = "";
 
+char *server = "10.1.41.223";
+int port = 1883;
+int var = 0;
+char datos[40];
+String resultS = "";
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje recibido [");
+  Serial.print(topic);
+  Serial.print("] ");
+  char payload_string[length + 1];
+  int resultI;
+  memcpy(payload_string, payload, length);
+  payload_string[length] = '\0';
+  resultI = atoi(payload_string);
+  var = resultI;
+  resultS = "";
+  for (int i=0;i<length;i++) {
+    resultS= resultS + (char)payload[i];
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!mqttClient.connected()) {
+    Serial.print("Intentando conectarse MQTT...");
+    if (mqttClient.connect("arduinoClient")) {
+      Serial.println("Conectado");
+      mqttClient.subscribe("Entrada/01");
+    } else {
+      Serial.print("Fallo, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" intentar de nuevo en 5 segundos");
+      delay(5000);
+    }
+  }
+}
+
 //Definicion de Variables
 
 float mq, Humidity, temperature;
-
 
 //Definir lapreparacion del codigo con SETUP
 void setup(){
   Serial.begin(115200);   
   Wire.begin();          
   mySensor.begin();
-
-  //The SCD4x has data ready every five seconds
-
+  
   //Connect to WiFi
   WiFi.mode(WIFI_STA);
-  wifiMulti.addAP(ssid, password);
+  WiFi.begin(ssid, password);
   Serial.println("Conectando a Wifi");
-  while(wifiMulti.run() != WL_CONNECTED){
+  while(WiFi.status() != WL_CONNECTED){
     Serial.println(".");
+    delay(500);
   }
   Serial.println();
   Serial.println("Wifi Conectado");
   Serial.println("Direccion IP: ");
   Serial.println(WiFi.localIP());
-  
+  mqttClient.setServer(server, port);
+  mqttClient.setCallback(callback);  
   
   Watchdog.enable(30000);
 
 }
 
-
 //Ciclo Loop para sensar constantemente
 void loop(){
   Watchdog.reset();
-  HTTPClient http;
-  Serial.println("[HTTP] Iniciando ... ");
-  //http.begin("http://172.1.1.19:8000/scd40");
-  http.begin("http://10.1.36.74:8000/scd40");
-  http.addHeader("Content-Type", "application/json");
-  Serial.println("[HTTP] POST...");
-  
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop();
   mySensor.readMeasurement();
 //Lectura de Sensores
   mq = mySensor.getCO2();    //MQ 135 Concentracion de Gases
   Humidity = mySensor.getHumidity(); // Humedad Relativa 0% - 100% (Punto de Rocio)
   temperature = mySensor.getTemperature();  // Temperatura 0°C - 120°C
-  String json = "{\"co2\":" + String(mq) + ",\"humedad\":" + String(Humidity) + ",\"temperatura\":" + String(temperature) + "}";
+  String json = "{\"co2\":" + String(mq) + ",\"humedad\":" + String(Humidity) + ",\"temperatura\":" + String(temperature) + ",\"SCD40\":" +"}";
   Serial.println(json);
-  int httpCode = http.POST(json);
-  String payload = http.getString();
-  Serial.println(httpCode);
-  Serial.println(payload);
-  http.end();
-  delay(1000); 
-  
+  mqttClient.publish("Salida/01", json);
+  delay(1000);
   }
