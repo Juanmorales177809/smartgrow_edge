d90071d88db33a9cdf396170e3e913f5ff324ca3
santiagoSuarez219 <santiago8628@gmail.com>
2023-10-02 16:58:13 -0500

    comunicacion backend ec y ph

--- src/EC_PH_LECTURE/EC_PH_LECTURE.ino
+++ src/EC_PH_LECTURE/EC_PH_LECTURE.ino
@@ -5,124 +5,106 @@
 #include <WiFi.h>
 #include "ph_grav.h"
 #include <ThingsBoard.h>
+#include <ArduinoJson.h>
+#include "WiFiModule.h"
+#include "MqttModule.h"
+#include "HttpModule.h"
 
 //----------------------------------------------------------------
 #include <EEPROM.h>
 #include "GravityTDS.h"
 
 #define TdsSensorPin A1
-GravityTDS gravityTds;
-float temperature = 25,tdsValue = 0;
-//----------------------------------------------------------------
+#define THINGSBOARD_MQTT_SERVER       "thingsboard.cloud"
+#define THINGSBOARD_MQTT_ACESSTOKEN   "9Bvzv2qpM9J6sveLvkLd"
+#define THINGSBOARD_MQTT_PORT         1883
 
-//----------------------------------------------------------------
+// WiFi
+const char *ssid = ""; // Nombre de la red WiFi
+const char *password = ""; // Contraseña de la red WiFi
+
+// MQTT
+const char* server = "200.122.207.134";
+const int mqtt_port = 8310;
+const int http_port = 8311;
+
+WiFiClient esp32Client;
+PubSubClient mqttClient(esp32Client);
+GravityTDS gravityTds;
+
+float temperature = 25,tdsValue = 0;
+
 Gravity_pH pH = Gravity_pH(32);
 Ezo_board EC = Ezo_board(100, "EC");      //create an EC circuit object who's address is 100 and name is "EC"
-//----------------------------------------------------------------
+
 void step1();  //forward declarations of functions to use them in the sequencer before defining them
 void step2();
-//----------------------------------------------------------------
 
 float TDS_float;                 //float var used to hold the float value of the total dissolved solids.
 float ph;                        //float var used to hold the float value of the specific PH.
 //----------------------------------------------------------------
 Sequencer2 Seq(&step1, 1000, &step2, 0);  //calls the steps in sequence with time in between them
 //==============================================================================
-#define THINGSBOARD_MQTT_SERVER       "thingsboard.cloud"
-#define THINGSBOARD_MQTT_ACESSTOKEN   "9Bvzv2qpM9J6sveLvkLd"
-#define THINGSBOARD_MQTT_PORT         1883
-//==============================================================================
-const char* ssid = "Convergentes";
-const char* password = "RedesConvergentes*#";
-//==============================================================================
-WiFiClient espClient;
-ThingsBoard tb(espClient);
+
+ThingsBoard tb(esp32Client);
+
 int status = WL_IDLE_STATUS;
 //==============================================================================
-char *server = "10.1.41.223";
-int port = 1883;
-int var = 0;
-char datos[40];
-String resultS = "";
-float     analog_ph;
-//=====================================(Mqtt)=========================================
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensaje recibido [");
-  Serial.print(topic);
-  Serial.print("] ");
-  char payload_string[length + 1];
-  int resultI;
-  memcpy(payload_string, payload, length);
-  payload_string[length] = '\0';
-  resultI = atoi(payload_string);
-  var = resultI;
-  resultS = "";
-  for (int i=0;i<length;i++) {
-    resultS= resultS + (char)payload[i];
-  }
-  Serial.println();
-}
+// MQTT
+const char* server = "200.122.207.134";
+const int mqtt_port = 8310;
+const int http_port = 8311;
 
-void reconnect() {
-  while (!mqttClient.connected()) {
-    Serial.print("Intentando conectarse MQTT...");
-    if (mqttClient.connect("arduinoClient")) {
-      Serial.println("Conectado");
-      mqttClient.subscribe("smartgrow");
-    } else {
-      Serial.print("Fallo, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" intentar de nuevo en 5 segundos");
-      delay(5000);
-    }
-  }
-}
-//==============================================================================
+float     analog_ph;
 String sensorstring = "";
 boolean sensor_string_complete = false;
+
+const char* sensor_id = "651b3c1a60ccd1c529a301d5"; // ID del sensor
+const unsigned long interval = 600000; // Intervalo de tiempo en milisegundos (10 min)
+unsigned long previousMillis = 0;
 
 void setup() {
   Wire.begin();                           //start the I2C
   Serial.begin(9600);                     //start the serial communication to the computer
   Seq.reset();                            //initialize the sequencer
-//-----------------------------------------------------------------------------
-gravityTds.setPin(TdsSensorPin);
-    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds.begin();  //initialization
-//-----------------------------------------------------------------------------
-  
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();  //initialization
   sensorstring.reserve(30);  
   pH.begin();
-  //Connect to WiFi
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("Conectando a Wifi");
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.println(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.println("Wifi Conectado");
-  Serial.println(WiFi.localIP());
-//==================================(Mqtt)============================================
-  mqttClient.setServer(server, port);
-  mqttClient.setCallback(callback);
-//==============================================================================  
+  WiFiModule::conectarWiFi(ssid, password);
+  mqttClient.setServer(server, mqtt_port);
+  mqttClient.setCallback(MqttModule::callback);  
 }
 
 void loop() {
   Seq.run();//run the sequncer to do the polling
 //==================================(Mqtt)============================================
   if (!mqttClient.connected()) {
-    reconnect();
+    MqttModule::conectarMQTT(mqttClient, server, mqtt_port);
   }
   mqttClient.loop();
-  String json = "{\"EC\":" + String(EC) + ",\"pH\":" + String(ph) + ",\"Sensor\":" + "EC_pH"+"}";
-  Serial.println(json);
-  mqttClient.publish("smartgrow", json.c_str());
-  delay(1000);
-//==============================================================================
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    StaticJsonDocument<200> jsonDocument; // Ajusta el tamaño según tus necesidades
+    jsonDocument["ph"] = ph;
+    jsonDocument["ec"] = EC;
+    jsonDocument["temperatura"] = 0.0;
+    jsonDocument["sensor"] = sensor_id;
+
+    // Serializar el JSON en una cadena
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
+
+    // Enviar datos
+    HttpModule::enviarDatosHTTP(server, http_port, jsonString.c_str());
+    String topic = "smartgrow/sensores/ph_ec";
+    MqttModule::enviarMensajeMQTT(mqttClient, jsonString, topic);
+    delay(1000);
+  }
 }
 
 void step1(){
@@ -140,9 +122,6 @@ void step1(){
       return;
       }
     }
-//================================================================================
-
-  
 }
 
 void step2(){
