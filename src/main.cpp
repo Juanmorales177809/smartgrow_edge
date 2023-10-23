813a25a09f74c27a7dce76009fdf6e7c3575296b
juanmorales177809 <mutaharmorales@gmail.com>
2023-10-23 17:10:12 -0500

    add_prepro

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,34 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <Wire.h>
 #include "SparkFun_SCD4x_Arduino_Library.h"
 #include <Adafruit_SleepyDog.h>
+
+#define TEL true
+#if TEL
+#include <WiFi.h>
+#include <Wire.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFiModule.h"
 #include "MqttModule.h"
 #include "HttpModule.h"
+#endif
 
-// WiFi
+//=======================================================================
+#if TEL
 const char *ssid = "Familia Morales"; // Nombre de la red WiFi
 const char *password = "2205631700"; // Contraseña de la red WiFi
-
-// MQTT
+// MQTT Server connection data 
 const char* server = "200.122.207.134";
 const int mqtt_port = 8310;
 const int http_port = 8311;
-
 WiFiClient esp32Client;
 PubSubClient mqttClient(esp32Client);
+
+#endif
+//=======================================================================
 SCD4x mySensor;
-
-float co2,temperature,humedad, VPD;
+float co2,temperatura,humedad, VPD;
 const char* sensor_id = "650dc7d640e0be7842fc4239"; // ID del sensor
-
 const unsigned long interval = 300000; // Intervalo de tiempo en milisegundos (5 min)
 unsigned long previousMillis = 0;
 
@@ -34,11 +38,13 @@ void setup()
   Serial.begin(115200);
   Wire.begin();
   mySensor.begin();
+  #if TEL
   WiFiModule::conectarWiFi(ssid, password);
   mqttClient.setServer(server, mqtt_port);
   mqttClient.setCallback(MqttModule::callback);
   Watchdog.enable(30000);
 }
+#endif
 float calcularVPD(float temperatura, float humedad){
   
   VPD = 0.611 * exp((17.27 * temperatura) / (temperatura + 237.3)) - (humedad / 100) * 0.611 * exp((17.27 * temperatura) / (temperatura + 237.3));//Calculo de VPD  con la formula de Buck 1996 (https://es.wikipedia.org/wiki/Presi%C3%B3n_de_vapor_de_agua)
@@ -46,6 +52,7 @@ float calcularVPD(float temperatura, float humedad){
 }
 void loop()
 {
+  #if TEL
   Watchdog.reset();
   // if (!mqttClient.connected()) {
   //   MqttModule::conectarMQTT(mqttClient, server, mqtt_port);
@@ -56,7 +63,10 @@ void loop()
   {
     previousMillis = currentMillis;
     mySensor.readMeasurement();
-    VPD = calcularVPD(mySensor.getTemperature(), mySensor.getHumidity());
+    temperatura = mySensor.getTemperature();
+    humedad = mySensor.getHumidity();
+    co2 = mySensor.getCO2();
+    VPD = calcularVPD(temperatura, humedad);
     StaticJsonDocument<200> jsonDocument; // Ajusta el tamaño según tus necesidades
     jsonDocument["co2"] = mySensor.getCO2();
     jsonDocument["temperatura"] = mySensor.getTemperature();
@@ -73,6 +83,8 @@ void loop()
     String topic = "smartgrow/sensores/scd40";
     MqttModule::enviarMensajeMQTT(mqttClient, jsonString, topic);
     delay(1000);
+    #endif
+    Serial.println("Datos: " + String(mySensor.getCO2()) + " " + String(mySensor.getTemperature()) + " " + String(mySensor.getHumidity()) + " " + String(VPD) + " " + String(sensor_id));
   }
 }
 
