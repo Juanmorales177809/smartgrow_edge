6620e864a805c217417abea776981e53a8fc166a
juanmorales177809 <mutaharmorales@gmail.com>
2023-10-24 08:56:54 -0500

    new_brach

--- src/main.cpp
+++ src/main.cpp
@@ -1,163 +1,90 @@
-/***************************************************************************
-
- ***************************************************************************/
 #include <Arduino.h>
-#include <WiFi.h>
-#include <Wire.h>
-#include <SPI.h>
-#include "Zanshin_BME680.h"
+#include "SparkFun_SCD4x_Arduino_Library.h"
+#include <Adafruit_SleepyDog.h>
+
 #define TEL false
 #if TEL
+#include <WiFi.h>
+#include <Wire.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include "WifiModule.h"
+#include "WiFiModule.h"
 #include "MqttModule.h"
 #include "HttpModule.h"
 #endif
-
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-
-// WiFi
+//=======================================================================
 #if TEL
-const char *ssid = "Convergentes"; // Nombre de la red WiFi
-const char *password = "RedesConvergentes*#"; // Contraseña de la red WiFi
-
-// MQTT
+const char *ssid = "Familia Morales"; // Nombre de la red WiFi
+const char *password = "2205631700"; // Contraseña de la red WiFi
+// MQTT Server connection data 
 const char* server = "200.122.207.134";
 const int mqtt_port = 8310;
 const int http_port = 8311;
-
 WiFiClient esp32Client;
 PubSubClient mqttClient(esp32Client);
 #endif
-//============================================================================  
-const u32_t SERIAL_SPEED{115200};
-BME680_Class BME680;
-const char* sensor_id = "651203198748ed5dd33b6d2e"; // ID del sensor
-const unsigned long interval = 300000; // Intervalo de tiempo en milisegundos (1 min)
+//=======================================================================
+SCD4x mySensor;
+float co2,temperatura,humedad, VPD;
+const char* sensor_id = "650dc7d640e0be7842fc4239"; // ID del sensor
+const unsigned long interval = 60000; // Intervalo de tiempo en milisegundos (5 min)
 unsigned long previousMillis = 0;
-//=============================================================================
-float altitude(const int32_t press, const float seaLevel = 1013.25);
-float altitude(const int32_t press, const float seaLevel) {
-  /*!
-  @brief     This converts a pressure measurement into a height in meters
-  @details   The corrected sea-level pressure can be passed into the function if it is known,
-             otherwise the standard atmospheric pressure of 1013.25hPa is used (see
-             https://en.wikipedia.org/wiki/Atmospheric_pressure) for details.
-  @param[in] press    Pressure reading from BME680
-  @param[in] seaLevel Sea-Level pressure in millibars
-  @return    floating point altitude in meters.
-  */
-  static float Altitude;
-  Altitude =
-      44330.0 * (1.0 - pow(((float)press / 100.0) / seaLevel, 0.1903));  // Convert into meters
-  return (Altitude);
-}
-
-
-void setup() {
-  Serial.begin(SERIAL_SPEED);
-  while (!Serial);
-  Serial.println(F("BME680 test"));
+//=======================================================================
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  mySensor.begin();
+  //=======================================================================
   #if TEL
   WiFiModule::conectarWiFi(ssid, password);
   mqttClient.setServer(server, mqtt_port);
   mqttClient.setCallback(MqttModule::callback);
+  Watchdog.enable(30000);
   #endif
-//==============================================================================
-  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
-    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
-    delay(5000);
-  }
-  BME680.setOversampling(TemperatureSensor, Oversample16);  // Use enumerated type values
-  BME680.setOversampling(HumiditySensor, Oversample16);     // Use enumerated type values
-  BME680.setOversampling(PressureSensor, Oversample16);     // Use enumerated type values
-  BME680.setIIRFilter(IIR4);  // Use enumerated type values
-  BME680.setGas(320, 150);  // 320�c for 150 milliseconds
-
-  // Set up oversampling and filter initialization
 }
-
-void loop() {
-  static int32_t  temp, humidity, pressure, gas;  // BME readings
-  static char     buf[16];                        // sprintf text buffer
-  static float    alt;                            // Temporary variable
-  static uint16_t loopCounter = 0;                // Display iterations
-  if (loopCounter % 25 == 0) {                    // Show header @25 loops
-    Serial.print(F("\nLoop Temp\xC2\xB0\x43 Humid% Press hPa   Alt m Air m"));
-    Serial.print(F("\xE2\x84\xA6\n==== ====== ====== ========= ======= ======\n"));  // "�C" symbol
-  }                                                     // if-then time to show headers
-  BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
-  if (loopCounter++ != 0) {                             // Ignore first reading, might be incorrect
-    sprintf(buf, "%4d %3d.%02d", (loopCounter - 1) % 9999,  // Clamp to 9999,
-            (int8_t)(temp / 100), (uint8_t)(temp % 100));   // Temp in decidegrees
-    Serial.print(buf);
-    sprintf(buf, "%3d.%03d", (int8_t)(humidity / 1000),
-            (uint16_t)(humidity % 1000));  // Humidity milli-pct
-    Serial.print(buf);
-    sprintf(buf, "%7d.%02d", (int16_t)(pressure / 100),
-            (uint8_t)(pressure % 100));  // Pressure Pascals
-    Serial.print(buf);
-    alt = altitude(pressure);                                                // temp altitude
-    sprintf(buf, "%5d.%02d", (int16_t)(alt), ((uint8_t)(alt * 100) % 100));  // Altitude meters
-    Serial.print(buf);
-    sprintf(buf, "%4d.%02d\n", (int16_t)(gas / 100), (uint8_t)(gas % 100));  // Resistance milliohms
-    Serial.print(buf);
-#if TEL
-  if (!mqttClient.connected()) {
-    MqttModule::conectarMQTT(mqttClient, server, mqtt_port);
-  }
+float calcularVPD(float temperatura, float humedad){
+  
+  VPD = 0.611 * exp((17.27 * temperatura) / (temperatura + 237.3)) - (humedad / 100) * 0.611 * exp((17.27 * temperatura) / (temperatura + 237.3));//Calculo de VPD  con la formula de Buck 1996 (https://es.wikipedia.org/wiki/Presi%C3%B3n_de_vapor_de_agua)
+  return VPD; 
+}
+void loop()
+{
+  //=======================================================================
+  #if TEL
   mqttClient.loop();
+  #endif
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval)
   {
     previousMillis = currentMillis;
+    mySensor.readMeasurement();
+    temperatura = mySensor.getTemperature();
+    humedad = mySensor.getHumidity();
+    co2 = mySensor.getCO2();
+    VPD = calcularVPD(temperatura, humedad);
+    #if TEL
     StaticJsonDocument<200> jsonDocument; // Ajusta el tamaño según tus necesidades
-    jsonDocument["temperatura"] = temp/100;
-    jsonDocument["humedad"] = humidity/100;
-    jsonDocument["altitud"] = alt;
+    jsonDocument["co2"] = mySensor.getCO2();
+    jsonDocument["temperatura"] = mySensor.getTemperature();
+    jsonDocument["humedad"] = mySensor.getHumidity();
+    jsonDocument["VPD"] = VPD;
     jsonDocument["sensor"] = sensor_id;
-
+    #endif
+    Serial.println("CO2:"+ String(co2) +" " + "Temperatura:" + String(temperatura) +" "+ "Humedad:" + String(humedad)+" " + "VPD:" + String(VPD) ); // Imprime los datos en el monitor serial
+    //Serial.println("Datos: " + String(mySensor.getCO2()) + " " + String(mySensor.getTemperature()) + " " + String(mySensor.getHumidity()) + " " + String(VPD) + " " + String(sensor_id));
+    #if TEL
     // Serializar el JSON en una cadena
     String jsonString;
     serializeJson(jsonDocument, jsonString);
 
     // Enviar datos
     HttpModule::enviarDatosHTTP(server, http_port, jsonString.c_str());
-    String topic = "smartgrow/sensores/bme680";
+    String topic = "smartgrow/sensores/scd40";
     MqttModule::enviarMensajeMQTT(mqttClient, jsonString, topic);
+    #endif
+    Watchdog.reset();
     delay(1000);
   }
-  #endif
-  Serial.print("Temperature = ");
-  Serial.print(temp/100);
-  Serial.println(" *C");
+}
 
-  Serial.print("Pressure = ");
-  Serial.print(pressure);
-  Serial.println(" hPa");
-
-  Serial.print("Humidity = ");
-  Serial.print(humidity/1000);
-  Serial.println(" %");
-
-  Serial.print("Gas = ");
-  Serial.print(gas);
-  Serial.println(" KOhms");
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(alt);
-  Serial.println(" m");
-
-  Serial.println();
-  delay(2000);
-      }
-    } // Add the missing curly brace here
-  // Add the missing curly brace here
-  
