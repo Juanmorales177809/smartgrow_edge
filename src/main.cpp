00457e4e2d6acf6fdb6dd170fac08cd7d42c7d0e
juanmorales177809 <mutaharmorales@gmail.com>
2023-10-25 09:23:51 -0500

    sensor2

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "SparkFun_SCD4x_Arduino_Library.h"
 #include <Adafruit_SleepyDog.h>
 
+#define ID1 false
+#define LOCAL false
 #define TEL true
 #if TEL
 #include <WiFi.h>
@@ -15,12 +17,20 @@
 
 //=======================================================================
 #if TEL
+#if LOCAL
+const char *ssid = "Convergentes"; // Nombre de la red WiFi
+const char *password = "RedesConvergentes*#"; // Contraseña de la red WiFi
+const char* server = "172.16.20.94"; // IP local del servidor MQTT
+const int mqtt_port = 1883;
+const int http_port = 3000;
+#endif
 const char *ssid = "Familia Morales"; // Nombre de la red WiFi
 const char *password = "2205631700"; // Contraseña de la red WiFi
-// MQTT Server connection data 
-const char* server = "200.122.207.134";
+const char* server = "200.122.207.134"; // IP publica del servidor MQTT
 const int mqtt_port = 8310;
 const int http_port = 8311;
+
+
 WiFiClient esp32Client;
 PubSubClient mqttClient(esp32Client);
 
@@ -28,8 +38,11 @@ PubSubClient mqttClient(esp32Client);
 //=======================================================================
 SCD4x mySensor;
 float co2,temperatura,humedad, VPD;
-const char* sensor_id = "650dc7d640e0be7842fc4239"; // ID del sensor
-const unsigned long interval = 300000; // Intervalo de tiempo en milisegundos (5 min)
+#if ID1
+const char* sensor_id = "650dc7d640e0be7842fc4239"; // ID del sensor 1
+#endif
+const char* sensor_id = "65391fa4700d51b6d681b3c5"; // ID del sensor 2
+const unsigned long interval = 1000; // Intervalo de tiempo en milisegundos (5 min)
 unsigned long previousMillis = 0;
 
 
@@ -54,9 +67,9 @@ void loop()
 {
   #if TEL
   Watchdog.reset();
-  // if (!mqttClient.connected()) {
-  //   MqttModule::conectarMQTT(mqttClient, server, mqtt_port);
-  // }
+  if (!mqttClient.connected()) {
+    MqttModule::conectarMQTT(mqttClient, server, mqtt_port);
+  }
   mqttClient.loop();
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval)
