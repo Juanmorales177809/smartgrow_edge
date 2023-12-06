de57cc3e97050bb64f8c08f17b37c601c483bf90
juanmorales177809 <mutaharmorales@gmail.com>
2023-12-05 19:14:23 -0500

    Update platformio.ini and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 #include <Adafruit_SleepyDog.h> //Include the watchdog library
 
 #define TEL true  // true para enviar datos a servidor, false para no enviar datos
-#define LOCAL true // true para servidor local, false para servidor remoto
+#define LOCAL false // true para servidor local, false para servidor remoto
 #define SENSORID1 true // true para sensor 1, false para sensor 2
-#define SCD4 false // true para sensor SCD40
-#define AS72 true // true para sensor AS7265X
+#define SCD4 true // true para sensor SCD40
+#define AS72 false // true para sensor AS7265X
 
 #if TEL
 #include "WifiModule.h"
@@ -24,17 +24,20 @@
 const char *ssid = "Convergentes"; // Nombre de la red WiFi
 const char *password = "RedesConvergentes*#"; // Contraseña de la red WiFi
 const char* server = "172.16.20.94"; // IP local del servidor MQTT
+const char* server2 = "172.16.20.115"; // IP local del servidor HTTP
 const int mqtt_port = 1883;
 const int http_port = 3000;
 #else
 const char *ssid = "Familia Morales"; // Nombre de la red WiFi
 const char *password = "2205631700"; // Contraseña de la red WiFi
 const char* server = "200.122.207.134"; // IP publica del servidor MQTT
+const char* server2 = "192.168.1.112"; // IP local del servidor HTTP
 const int http_port = 8311;
 const int mqtt_port = 8310;
 #endif
 WifiModule wifiModule(ssid, password);
 HttpModule httpClient(server, http_port);
+HttpModule httpClient2(server2, http_port);
 
 #endif
 //=======================================================================
@@ -51,7 +54,7 @@ SCD40Sensor SCD40(sensor_id);
 AS7265xModule AS7265X(sensor_id);
 #endif
 //=======================================================================
-const unsigned long interval = 300000; // Intervalo de tiempo en milisegundos (5 min)
+const unsigned long interval = 5000; // Intervalo de tiempo en milisegundos (5 min)
 unsigned long previousMillis = 0;
 //=======================================================================
 void setup()
@@ -89,10 +92,12 @@ void loop()
     #if SCD4
     String jsonString = SCD40.buildJson();
     httpClient.enviarDatosHTTP(jsonString.c_str(), "scd40");
+    httpClient2.enviarDatosHTTP(jsonString.c_str(), "scd40");
     #endif
     #if AS72
     String jsonString2 = AS7265X.buildJson();
     httpClient.enviarDatosHTTP(jsonString2.c_str(), "as7265x");
+    httpClient2.enviarDatosHTTP(jsonString2.c_str(), "as7265x");
     #endif
     delay(1000);
     #endif
